614ff6c1a48369ad35234483031a82b5d8e6a937
RoksanaPR1 <rocksana0110@gmail.com>
2021-09-25 10:27:57 +0200

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.print("Roksana Balcerzak");
+        System.out.println(" "+"ININ3"+"_"+"PR"+"1"+"."+"1");
         System.out.print("To jest mój pierwszy");
         System.out.println(" "+"program"+" "+"w"+" "+"języku"+" "+"Java");
         System.out.printf("A %s zaczynamy z %d przykładem. \n", "więc", 1);
